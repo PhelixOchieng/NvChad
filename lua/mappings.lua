e26d2c056eb7d91fb064c737cbff8d9603f3d5d4
Phelix <dev.phelixochieng@gmail.com>
2024-04-15 20:35:11 +0300

    feat (core): Add custom mappings and options

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,5 +1,13 @@
 require "nvchad.mappings"
 
+-- Modes
+--   normal_mode = "n",
+--   visual_mode = "v",
+--   insert_mode = "i",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
 -- add yours here
 
 local map = vim.keymap.set
@@ -7,4 +15,26 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- Comments
+map({ "n", "i" }, "<C-_>", function()
+  require("Comment.api").toggle.linewise.current()
+end, { desc = "Comment Line Toggle" })
+map(
+  'v',
+  "<C-_>",
+  "<ESC><cmd>lua require('Comment.api').toggle.blockwise(vim.fn.visualmode())<CR>",
+  { desc = "Toggle Block Comment" }
+)
+
+-- Movement
+map("n", "<A-j>", "<Esc>:m .+1<CR>==", { desc = "Move block down"} )
+map("n", "<A-k>", "<Esc>:m .-2<CR>==", { desc = "Move block up"} )
+map("v", "<A-j>", ":m '> +1<CR>==gv=gv", { desc = "Move line(s) down"} )
+map("v", "<A-k>", ":m '> -2<CR>==gv=gv", { desc = "Move line(s) up"} )
+
+map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- Folding
+map("n", "zR", function ()
+  require('ufo').openAllFolds()
+end, { desc = "Open all folds" })
--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,22 @@
 require "nvchad.options"
 
 -- add yours here!
+local options = {
+  wrap = false,
+  cursorlineopt = "both",    -- to enable cursorline!
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+  colorcolumn = "100",
+  relativenumber = true,
+  scrolloff = 8,
+  sidescrolloff = 8,
+
+  -- Folding
+  foldcolumn = "0",
+  foldlevel = 99,
+  foldlevelstart = 99,
+  foldenable = true,
+}
+
+for key, value in pairs(options) do
+  vim.opt[key] = value
+end
