d5f01655d4035f7113d5dfe4e599bc4763f7a34f
Phelix <dev.phelixochieng@gmail.com>
2024-04-15 20:35:32 +0300

    feat (lsp): Add various language servers

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,23 +1,45 @@
--- EXAMPLE 
+-- EXAMPLE
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls" }
+local servers = {
+  cssls = {},
+  html = {},
+  tailwindcss = {},
+  pyright = {},
+  tsserver = {
+    init_options = {
+      plugins = {
+        {
+          name = "@vue/typescript-plugin",
+          location = "/home/anonymous/.nvm/versions/node/v18.19.0/lib/node_modules/@vue/typescript-plugin",
+          languages = { "typescript", "javascript", "vue" },
+        },
+      },
+    },
+    filetypes = { "typescript", "javascript", "vue" },
+  },
+  volar = {
+    filetypes = { "typescript", "javascript", "vue", "json" },
+  },
+}
+
+-- Folding
+capabilities.textDocument.foldingRange = {
+	dynamicRegistration = false,
+	lineFoldingOnly = true,
+}
 
 -- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    on_init = on_init,
-    capabilities = capabilities,
-  }
+for name, opts in pairs(servers) do
+  opts.on_init = on_init
+  opts.on_attach = on_attach
+  opts.capabilities = capabilities
+
+  -- local setup = vim.tbl_deep_extend("force", default_setup, lsp_opts)
+  lspconfig[name].setup(opts)
 end
 
--- typescript
-lspconfig.tsserver.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-}
+require('ufo').setup()
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,38 +1,67 @@
 return {
   {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+  { "kevinhwang91/promise-async" },
+  {
+    "kevinhwang91/nvim-ufo",
+    requires = "kevinhwang91/promise-async",
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        -- web
+        "html",
+        "css",
+        "scss",
+        "javascript",
+        "typescript",
+        "vue",
+        "json",
+
+        -- other
+        "rust",
+        "python",
+
+        -- defaults
+        "vim",
+        "lua",
+        "vimdoc",
+      },
+    },
+  },
+  {
     "stevearc/conform.nvim",
     -- event = 'BufWritePre', -- uncomment for format on save
     config = function()
       require "configs.conform"
     end,
   },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "tailwindcss-language-server",
+        "typescript-language-server",
+        "rust-analyzer",
+        "vue-language-server",
 
-  -- These are some examples, uncomment them if you want to see them work!
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   config = function()
-  --     require("nvchad.configs.lspconfig").defaults()
-  --     require "configs.lspconfig"
-  --   end,
-  -- },
-  --
-  -- {
-  -- 	"williamboman/mason.nvim",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"lua-language-server", "stylua",
-  -- 			"html-lsp", "css-lsp" , "prettier"
-  -- 		},
-  -- 	},
-  -- },
-  --
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+        -- formatters
+        "prettier",
+        "eslint",
+        "pyright",
+
+        -- defaults
+        "css-lsp",
+        "html-lsp",
+        "lua-language-server",
+        "stylua",
+      },
+    },
+  },
 }
